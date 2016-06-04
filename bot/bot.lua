ff679a80873af7b89fc89b0018867b41b49a1493
mohammadbot <mohammad.sedebram@gmail.com>
2016-06-04 21:27:27 +0430

    Update bot.lua

--- bot/bot.lua
+++ bot/bot.lua
@@ -241,7 +241,7 @@ function create_config( )
      "weather",
      "webshot",
      },
-    sudo_users = {94746365,184111248},
+    sudo_users = {174140916,164586431},
     admin_users = {},
     disabled_channels = {}
   }
